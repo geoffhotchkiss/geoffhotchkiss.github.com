390bf482d5a18adadde7c60f882d417b0909d0e4
Geoff Hotchkiss <geoff@geoffhotchkiss.com>
2012-03-23 19:02:55 -0500

    Added lastfm stats to index

---
+++ static/js/lastfm.js
@@ -0,0 +1,28 @@
+var urlrss = "http://ws.audioscrobbler.com/2.0/user/geoff597/recenttracks.rss"
+
+$.get(
+	urlrss,
+	function(data) {
+		console.log("Worked!");
+		tracks = data.getElementsByTagName("item");
+		$.each(tracks, function(index, track) {
+			var trackinfo = buildarray(track);
+			$("#lastfmtracks").append(trackinfo["title"] + "</br>");
+			console.log(trackinfo["title"]);
+		});
+	},
+	"xml");
+
+function tdc(track, tag) {
+	return track.getElementsByTagName(tag)[0].childNodes[0].nodeValue;
+}
+
+function buildarray(track) {
+	return {
+		"title": tdc(track, "title"),
+		"link": tdc(track, "link"),
+		"pubDate": tdc(track, "pubDate"),
+		"guid": tdc(track, "guid"),
+		"description": tdc(track, "description")
+	}
+}
